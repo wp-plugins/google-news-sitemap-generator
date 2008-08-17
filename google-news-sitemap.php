2a5efe7bd4f9a5aeda081a698091817f12fd81a1
chris.jinks <chris.jinks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-08-17 16:47:47 +0000

    git-svn-id: https://plugins.svn.wordpress.org/google-news-sitemap-generator/tags/v1.1@60348 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ google-news-sitemap.php
@@ -0,0 +1,153 @@
+<?
+/* 
+Plugin Name: Google News Sitemap
+Plugin URI: http://southcoastwebsites.com/wordpress/
+Version: v1.1
+Author: <a href="http://southcoastwebsites.com/wordpress/">Chris Jinks</a>
+Description: Basic XML sitemap generator for submission to Google News 
+
+
+Installation:
+==============================================================================
+	1. Upload `google-news-sitemap-generator` directory to the `/wp-content/plugins/` directory
+	2. Activate the plugin through the 'Plugins' menu in WordPress
+	3. Create file "google-news-sitemap.xml" in your root directory and CHMOD so it is writable
+
+
+Release History:
+==============================================================================
+	2008-08-04		v1.00		First release
+	2008-08-17		v1.1		Compatible with new Wordpress database taxonomy (>2.3)
+
+
+
+ 
+*/
+
+/*  Copyright 2008 Chris Jinks / David Stansbury
+	
+	Original concept: David Stansbury - http://www.kb3kai.com/david_stansbury/
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+function get_category_keywords($newsID)
+{
+	global $wpdb;
+	
+	
+	//Old Wordpress database taxonomy
+	if ( get_bloginfo('version') > 2.3 )
+		{
+			$categories = $wpdb->get_results("SELECT category_id FROM $wpdb->post2cat WHERE post_id=$newsID");
+			$i = 0;
+			$categoryKeywords = "";
+			foreach ($categories as $category)
+			{
+				if ($i>0){$categoryKeywords.= ", ";} //Comma seperator
+				$categoryKeywords.= get_catname($category->category_id); //ammed string
+				$i++;
+			}
+		}
+		
+	//New >2.3 Wordpress taxonomy	
+	else
+		{
+			$categories = $wpdb->get_results("
+				SELECT wp_terms.name FROM `wp_term_relationships`,  `wp_term_taxonomy`,  `wp_terms`
+				WHERE wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
+				AND wp_term_taxonomy.term_id =  wp_terms.term_id
+				AND wp_term_relationships.object_id = $newsID
+				AND wp_term_taxonomy.taxonomy = 'category'");
+			$i = 0;
+			$categoryKeywords = "";
+			foreach ($categories as $category)
+			{
+				if ($i>0){$categoryKeywords.= ", ";} //Comma seperator
+				$categoryKeywords.= $category->name; //ammed string
+				$i++;
+			}
+		}
+	
+	
+	return $categoryKeywords; //Return post category names as keywords
+}
+
+function write_google_news_sitemap() 
+{
+
+	global $wpdb;
+	// Fetch options from database
+	$permalink_structure = $wpdb->get_var("SELECT option_value FROM $wpdb->options 
+					WHERE option_name='permalink_structure'");
+	$siteurl = $wpdb->get_var("SELECT option_value FROM $wpdb->options
+				WHERE option_name='siteurl'");
+
+	// Output XML header
+	
+	
+	// Begin urlset
+	$xmlOutput = "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\"
+			xmlns:news=\"http://www.google.com/schemas/sitemap-news/0.9\">\n";
+	
+	// Select all posts
+	//$rows = $wpdb->get_results("SELECT ID, post_date_gmt
+	//       				FROM $wpdb->posts WHERE post_status='publish'");
+	
+	
+	//Limit to last 3 days, 1000 items					
+	$rows = $wpdb->get_results("SELECT ID, post_date_gmt
+						FROM $wpdb->posts 
+						WHERE post_status='publish' AND (DATEDIFF(CURDATE(), post_date_gmt)<=3) 
+						ORDER BY post_date_gmt DESC
+						LIMIT 0, 1000");					
+	
+	// Output sitemap data
+	foreach($rows as $row){
+		$xmlOutput.= "\t<url>\n";
+		$xmlOutput.= "\t\t<loc>";
+		$xmlOutput.= get_permalink($row->ID);
+		$xmlOutput.= "</loc>\n";
+		$xmlOutput.= "\t\t<news:news>\n";
+		$xmlOutput.= "\t\t<news:publication_date>";
+		$thedate = substr($row->post_date_gmt, 0, 10);
+		$thetime = substr($row->post_date_gmt, 11, 20);
+		$xmlOutput.= $thedate . 'T' . $thetime . 'Z';
+		$xmlOutput.= "</news:publication_date>\n";
+		$xmlOutput.= "\t\t<news:keywords>";
+		
+		//Use the categories for keywords
+		$xmlOutput.= get_category_keywords($row->ID);
+		
+		$xmlOutput.= "</news:keywords>\n"; 
+		$xmlOutput.= "\t\t</news:news>\n";
+		$xmlOutput.= "\t</url>\n";
+	}
+	
+	// End urlset
+	$xmlOutput.= "</urlset>\n";
+	$xmlOutput.= "<!-- Last build time: ".date("F j, Y, g:i a")."-->";
+	
+	$xmlFile = "../google-news-sitemap.xml";
+	$fp = fopen($xmlFile, "w+"); // open the cache file "news-sitemap.xml" for writing
+	fwrite($fp, $xmlOutput); // save the contents of output buffer to the file
+	fclose($fp); // close the file
+}
+
+add_action('publish_post', 'write_google_news_sitemap');
+add_action('save_post', 'write_google_news_sitemap');
+add_action('delete_post', 'write_google_news_sitemap');
+?>
---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Google News Sitemap Generator ===
+
+Contributors: Chris Jinks, David Stansbury
+Donate link: http://southcoastwebsites.com/wordpress, http://www.kb3kai.com/david_stansbury
+Tags: Google News, Sitemap, XML
+
+Basic XML sitemap generator for submission to Google News.
+
+== Description ==
+
+Automatically generates a Google News XML sitemap for submission online via Webmaster Tools.
+
+Follows Google guidelines:
+
+* 1000 item limit
+* Posts from the last 3 days
+
+Keywords for sitemap are auto generated from post category.
+
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `google-news-sitemap-generator` directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Create file "google-news-sitemap.xml" in your root directory and CHMOD so it is writable
+
